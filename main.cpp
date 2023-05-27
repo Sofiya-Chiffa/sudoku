2a2fa5db953060973faa0eadbeef16654bcac000
Sofiya-Chiffa <94921852+Sofiya-Chiffa@users.noreply.github.com>
2023-05-27 17:36:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,72 +4,64 @@
 
 using namespace std;
 
-int make_numb(int k, int i, int j, int arr[9][3][3]){
-    int numb[9] = {};
-    int numb_ok[9] = {};
-    for (int ni=0;ni<3;++ni){
-        for (int nj=0;nj<3;++nj){
-            if (arr[k][ni][nj] != 0){
-                numb[arr[k][ni][nj] - 1] = 1;
-            }
-        }
-    }
-    
-    //for (int nk=0;nk<9;++nk){
-    //    for (int nj=0;nj<3;++nj){
-    //        if (arr[nk][i][nj] != 0){
-    //            numb[arr[nk][i][nj] - 1] = 1;
-    //        }
-    //    }
-    //}
-    //
-    //for (int nk=0;nk<9;++nk){
-    //    for (int ni=0;ni<3;++ni){
-    //        if (arr[nk][ni][j] != 0){
-    //            numb[arr[nk][ni][j] - 1] = 1;
-   //         }
-    //    }
-    //}
-    
-    int len = 0;
-    int ij = 0;
-    for (int ni=0;ni<9;++ni){
-        if (numb[ni] == 0){
-            numb_ok[ij] = ni + 1;
-            //cout << numb_ok[ij] << " " << len << "\n";
-            ij += 1;
-            len+=1;
-        }
-    } 
-    srand(time(0));
-	int num =  numb_ok[rand() % len];
-    return num;
-}
 
 int main()
 {
-    int arr_user[9][3][3] = {};
-    int arr_pc[9][3][3] = {};
-    for (int k=0;k<3;++k){
+    int arr_user[9][9] = {};
+    
+    
+    srand(time(0));
+    int arr_pc[9][9] = {};
+    for (int i=0;i<9;++i){
+        for (int j=0;j<9;++j){
+            arr_pc[i][j] = ((j + i*3 + i/3) % (9) + 1);
+        }
+    }
+    for (int k;k<2;++k){
+        int a1 = rand() % 3;
+        int b1 = (a1 + 1) % 3;
+        int b2 = (a1 + 2) % 3;
+        int me = 0;
         for (int i=0;i<3;++i){
-            for (int j=0;j<3;++j){
-                arr_pc[k][i][j] = make_numb(k, i, j, arr_pc);
+            for (int j=0;j<9;++j){
+                me = arr_pc[i + (b1 * 3)][j];
+                arr_pc[i + (b1 * 3)][j] = arr_pc[i + (b2 * 3)][j];
+                arr_pc[i + (b2 * 3)][j] = me;
             }
         }
     }
-
+    
+    for (int k;k<2;++k){
+        for (int n;n<3;++n){
+            int a1 = rand() % 3;
+            int b1 = (a1 + 1) % 3;
+            int b2 = (a1 + 2) % 3;
+            int me = 0;
+            for (int j=0;j<9;++j){
+                me = arr_pc[n * 3 + b1][j];
+                arr_pc[n * 3 + b1][j] = arr_pc[n * 3 + b2][j];
+                arr_pc[n * 3 + b2][j] = me;
+            }
+        }
+    }
+    
+    
+    
+    
+    
     for (int i=0;i<9;++i){
         if (i % 3 == 0){
             cout << "\n";
         }
-        for (int k=0;k<3;++k){
-            for (int j=0;j<3;++j){
-                cout << arr_pc[k][i % 3][j] << " ";
+        for (int j=0;j<9;++j){
+            if (j % 3 == 0){
+                cout << "  ";
             }
-            cout << "  ";
+            cout << arr_pc[i][j] << " ";
         }
         cout << "\n";
     }
+    cout << "\n";
 
     return 0;
 }
